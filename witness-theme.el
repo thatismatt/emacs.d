ad78f8fcc37b89f693e6253a5c40e872ac97f22d
Matt Lee <thatismatt@gmail.com>
2014-01-21 22:36:37 +0000

    Theme tweaks.

--- witness-theme.el
+++ witness-theme.el
@@ -16,33 +16,38 @@
 
 (custom-theme-set-variables
   'witness
-  '(linum-format "%3i "))
+  '(linum-format "%4i "))
 
-(let ((*background*         "#011")
-      (*normal*             "#AAA")
-      (*current-line*       "#111")
-      (*cursor-underscore*  "#999")
-      (*visual-selection*   "#135")
+(let* ((*background*         "#011")
+       (*normal*             "#AAA")
+       (*current-line*       "#111")
+       (*cursor-underscore*  "#999")
+       (*visual-selection*   "#135")
+       (*visual-selection-1* "#012")
+       (*visual-selection-2* "#123")
 
-      (*line-number*        "#333")
-      (*line-fg*            "#000")
+       (*line-number*        "#333")
+       (*line-fg*            "#888")
 
-      (*mode-line-bg*       "#333")
-      (*mode-inactive-bg*   "#222")
-      (*mode-line-fg*       "#888")
-      (*mode-inactive-fg*   "#555")
+       (*mode-line-bg*       "#333")
+       (*mode-inactive-bg*   "#222")
+       (*mode-line-fg*       "#888")
+       (*mode-inactive-fg*   "#555")
 
-      (*comments*           "#555")
-      (*keywords*           "#ba1")
-      (*constant*           "#83e")
-      (*type-face*          "#c0a")
-      (*method-declaration* "#09f")
-      (*number*             "#00f")
-      (*operators* )
-      (*warning*            "#b30")
-      (*regexp*             "#A36")
-      (*string*             "#080")
-      (*variable*           "#06c"))
+       (*red*                "#810")
+       (*green*              "#080")
+
+       (*comments*           "#555")
+       (*keywords*           "#ba1")
+       (*constant*           "#c0a")
+       (*type-face*          "#349")
+       (*method-declaration* "#09f")
+       (*number*             "#00f")
+       (*operators*          "#83e")
+       (*warning*            *red*)
+       (*regexp*             "#a36")
+       (*string*             *green*)
+       (*variable*           "#06c"))
 
   (custom-theme-set-faces
    'witness
@@ -50,12 +55,13 @@
    `(bold ((t (:bold t))))
    `(button ((t (:foreground ,*keywords* :underline t))))
    `(default ((t (:background ,*background* :foreground ,*normal*))))
-   `(header-line ((t (:background ,*mode-line-bg* :foreground ,*normal*)))) ;; info header
+   `(header-line ((t (:background ,*mode-line-bg* :foreground ,*normal*))))
    `(highlight ((t (:background ,*current-line*))))
    `(highlight-face ((t (:background ,*current-line*))))
-   `(hl-line ((t (:bold t)))) ;; (:background ,*current-line*)
+   `(hl-line ((t (:bold t))))
    `(info-xref ((t (:foreground ,*keywords* :underline t))))
    `(region ((t (:background ,*visual-selection*))))
+   `(secondary-selection ((t (:background ,*visual-selection-2*))))
    `(underline ((nil (:underline t))))
 
    ;; font-lock
@@ -80,13 +86,13 @@
 
    ;; GUI
    `(fringe ((t (:background ,*background*))))
-   `(linum ((t (:background ,*line-number* :foreground ,*line-fg*))))
+   `(linum ((t (:background ,*line-number* :foreground ,*line-fg* :height 90 :bold t))))
    `(minibuffer-prompt ((t (:foreground ,*variable*))))
    `(mode-line ((t (:background ,*mode-line-bg* :foreground ,*mode-line-fg*))))
    `(mode-line-inactive ((t (:background ,*mode-inactive-bg* :foreground ,*mode-inactive-fg*))))
    `(cursor ((t (:background ,*cursor-underscore*))))
    `(text-cursor ((t (:background ,*cursor-underscore*))))
-   `(vertical-border ((t (:foreground ,*background*)))) ;; between splits
+   `(vertical-border ((t (:foreground ,*background*))))
 
    ;; show-paren
    `(show-paren-mismatch ((t (:background ,*warning* :foreground ,*normal* :weight bold))))
@@ -96,6 +102,12 @@
    `(isearch ((t (:background ,*regexp* :foreground ,*visual-selection*))))
    `(isearch-fail ((t (:background ,*warning*))))
    `(lazy-highlight ((t (:background ,*operators* :foreground ,*visual-selection*))))
+
+   ;; diff
+   `(diff-header ((t (:background ,*visual-selection-1*))))
+   `(diff-file-header ((t (:bold t))))
+   `(diff-added ((t (:foreground ,*green*))))
+   `(diff-removed ((t (:foreground ,*red*))))
    ))
 
 (provide-theme 'witness)
