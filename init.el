223357a9ddcf227cebe6047e8bcd00d7e3c37e47
Matt Lee <thatismatt@gmail.com>
2014-01-21 15:43:18 +0000

    M-x start!

---
+++ init.el
@@ -0,0 +1,175 @@
+(prelude-require-packages
+ '(auto-complete
+   yasnippet
+   js2-mode
+   js2-refactor
+   skewer-mode
+   ac-js2))
+
+(disable-theme 'zenburn)
+
+(setq custom-theme-load-path
+      (cons "~/.emacs.d/personal" custom-theme-load-path))
+(load-theme 'witness t)
+
+(setq prelude-guru nil)
+(setq prelude-whitespace nil)
+
+;; font
+(set-face-attribute 'default nil :height 110)
+(set-face-attribute 'default nil :family "Liberation Mono")
+;; (set-face-attribute 'default nil :family "DejaVu Sans Mono")
+
+;(set-face-attribute 'org-level-1 nil :height 110)
+;(set-face-attribute 'org-level-2 nil :height 110)
+;(set-face-attribute 'org-todo nil :height 110)
+;(set-face-attribute 'org-done nil :height 110)
+
+;; cursor - bar instead of a block
+(setq default-cursor-type '(bar . 2))
+
+;; window title - include file's full path
+(setq frame-title-format
+      '("" invocation-name " - "
+        (:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name))
+                 "%b"))))
+
+;; kill whole line
+(setq kill-whole-line t)
+
+;; turn off newline on save
+;(setq require-final-newline nil)
+
+;; window dedication
+(defun toggle-window-dedicated ()
+  "Toggle whether the current active window is dedicated or not"
+  (interactive)
+  (message
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window
+                                 (not (window-dedicated-p window))))
+       "Window '%s' is dedicated"
+     "Window '%s' is normal")
+   (current-buffer)))
+
+;; enable stuff
+;(put 'upcase-region 'disabled nil)
+;(put 'narrow-to-region 'disabled nil)
+;(put 'narrow-to-page 'disabled nil)
+
+;; rainbow delimiters
+;; colors
+(setq rainbow-delimiters-max-face-count 7)
+(set-face-foreground 'rainbow-delimiters-depth-1-face "#800")
+(set-face-foreground 'rainbow-delimiters-depth-2-face "#070")
+(set-face-foreground 'rainbow-delimiters-depth-3-face "#23b")
+(set-face-foreground 'rainbow-delimiters-depth-4-face "#870")
+(set-face-foreground 'rainbow-delimiters-depth-5-face "#808")
+(set-face-foreground 'rainbow-delimiters-depth-6-face "#068")
+(set-face-foreground 'rainbow-delimiters-depth-7-face "#840")
+
+;; vcs - disable on windows as too slow
+;(when (eq system-type 'windows-nt)
+;  (setq vc-handled-backends ())
+;  (global-git-gutter-mode 0))
+
+;; nrepl tweaks
+;; (setq nrepl-popup-stacktraces-in-repl t)
+;; default: (setq nrepl-popup-stacktraces-in-repl nil)
+
+;; auto complete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; linum
+(add-hook 'prog-mode-hook 'linum-mode)
+
+;; yas
+(add-hook 'prog-mode-hook
+          '(lambda () (yas-minor-mode)))
+(setq yas-snippet-dirs '("~/.emacs.d/personal/snippets" "~/.emacs.d/elpa/yasnippet-20140106.1009/snippets"))
+;(setq yas-snippet-dirs '("~/.emacs.d/personal/snippets"))
+(setq yas-snippet-dirs (cons "~/.emacs.d/personal/snippets" (cdr yas-snippet-dirs)))
+;; HACK - relies on the second element of yas-snippets-dir being the snippets from melpa
+;; NOTE - yas fails to load if a dir that doesn't exist is in yas-snippets-dir
+;;        ~/.emacs.d/snippets is in there by default, and doesn't exist
+;; (yas-reload-all)
+
+;; speedbar
+(require 'speedbar)
+(setq speedbar-show-unknown-files t)
+(setq speedbar-use-images nil)
+(add-to-list 'speedbar-frame-parameters '(width . 30))
+(defun speedbar-kill ()
+  (interactive)
+  (if (get-buffer " SPEEDBAR")
+      (kill-buffer " SPEEDBAR")))
+
+;; magit
+;(setq magit-diff-options '("--ignore-space-change"))
+;(setq magit-status-buffer-switch-function 'switch-to-buffer)
+;(setq magit-server-window-for-commit nil)
+
+;; org
+(setq org-todo-keywords '((type "TODO" "DOING" "DONE" "|" "POSTPONED")))
+(setq org-todo-keyword-faces
+      '(("TODO" :foreground "#d77" :weight bold)
+        ("DONE" :foreground "#7d7" :weight bold)
+        ("DOING" :foreground "#ed7" :weight bold)
+        ("POSTPONED" :foreground "#aa9" :weight bold)))
+(setq org-startup-folded nil)
+(setq org-log-done nil)
+
+;; hl line
+;(set-face-background 'hl-line "#323")
+;(set-face-attribute 'hl-line nil :underline nil)
+
+;; whitespace mode
+(setq  whitespace-style '(face tabs empty trailing))
+
+;; js2-mode
+(set-face-foreground 'js2-function-param "#0c0")
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq js2-global-externs '("$" "angular" "_" "Highcharts" "google" "d3"))
+(require 'js2-refactor)
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
+;; skewer mode
+(add-hook 'js2-mode-hook 'skewer-mode)
+(setq httpd-port 8081) ; default is 8080
+
+;; auto complete for js2
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+(setq ac-js2-evaluate-calls t)
+
+;; auto mode
+(add-to-list 'auto-mode-alist '("\\.\\(jspf?\\|tag\\)$" . html-mode))
+
+;; window & buffer switching
+(defun matt-normal-buffer-switch (f)
+  "Switch buffers with `f` until the `buffer-name` doesn't start with a \"*\"."
+  (funcall f)
+  (if (equal "*" (substring (buffer-name) 0 1))
+      (progn
+        (message "Skipped buffer %s" (buffer-name))
+        (matt-normal-buffer-switch f))))
+(define-key global-map (kbd "S-<menu>")
+  (lambda () (interactive) (matt-normal-buffer-switch 'previous-buffer)))
+(define-key global-map (kbd "<menu>")
+  (lambda () (interactive) (matt-normal-buffer-switch 'next-buffer)))
+(define-key global-map (kbd "C-<menu>") 'other-window)
+
+(defun matt-just-one-space-multiline ()
+  (interactive)
+  (just-one-space -1))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; KEYS
+
+;; kill buffer - without confirmation
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "M-SPC") 'matt-just-one-space-multiline)
---
+++ witness-theme.el
@@ -0,0 +1,103 @@
+;;; witness-theme.el --- A dark theme for Emacs.
+;; Copyright (C) 2014 Matt Lee
+;; Author: Matt Lee
+;; URL: http://github.com/thatismatt
+;; License: MIT
+
+;;; Commentary:
+;; A dark theme.
+
+;;; Code:
+
+(unless (>= 24 emacs-major-version)
+  (error "Requires Emacs 24 or later"))
+
+(deftheme witness "A dark color theme for Emacs based on spolsky from sublime-themes")
+
+(custom-theme-set-variables
+  'witness
+  '(linum-format "%3i "))
+
+(let ((*background*         "#011")
+      (*normal*             "#AAA")
+      (*current-line*       "#111")
+      (*cursor-underscore*  "#999")
+      (*visual-selection*   "#135")
+
+      (*line-number*        "#333")
+      (*line-fg*            "#000")
+
+      (*mode-line-bg*       "#333")
+      (*mode-inactive-bg*   "#222")
+      (*mode-line-fg*       "#888")
+      (*mode-inactive-fg*   "#555")
+
+      (*comments*           "#555")
+      (*keywords*           "#ba1")
+      (*constant*           "#83e")
+      (*type-face*          "#c0a")
+      (*method-declaration* "#09f")
+      (*number*             "#00f")
+      (*operators* )
+      (*warning*            "#b30")
+      (*regexp*             "#A36")
+      (*string*             "#080")
+      (*variable*           "#06c"))
+
+  (custom-theme-set-faces
+   'witness
+
+   `(bold ((t (:bold t))))
+   `(button ((t (:foreground ,*keywords* :underline t))))
+   `(default ((t (:background ,*background* :foreground ,*normal*))))
+   `(header-line ((t (:background ,*mode-line-bg* :foreground ,*normal*)))) ;; info header
+   `(highlight ((t (:background ,*current-line*))))
+   `(highlight-face ((t (:background ,*current-line*))))
+   `(hl-line ((t (:bold t)))) ;; (:background ,*current-line*)
+   `(info-xref ((t (:foreground ,*keywords* :underline t))))
+   `(region ((t (:background ,*visual-selection*))))
+   `(underline ((nil (:underline t))))
+
+   ;; font-lock
+   `(font-lock-builtin-face ((t (:foreground ,*operators*))))
+   `(font-lock-comment-delimiter-face ((t (:foreground ,*comments*))))
+   `(font-lock-comment-face ((t (:foreground ,*comments*))))
+   `(font-lock-constant-face ((t (:foreground ,*constant*))))
+   `(font-lock-doc-face ((t (:foreground ,*string*))))
+   `(font-lock-doc-string-face ((t (:foreground ,*string*))))
+   `(font-lock-function-name-face ((t (:foreground ,*method-declaration*))))
+   `(font-lock-keyword-face ((t (:foreground ,*keywords*))))
+   `(font-lock-negation-char-face ((t (:foreground ,*warning*))))
+   `(font-lock-number-face ((t (:foreground ,*number*))))
+   `(font-lock-preprocessor-face ((t (:foreground ,*keywords*))))
+   `(font-lock-reference-face ((t (:foreground ,*constant*))))
+   `(font-lock-regexp-grouping-backslash ((t (:foreground ,*regexp*))))
+   `(font-lock-regexp-grouping-construct ((t (:foreground ,*regexp*))))
+   `(font-lock-string-face ((t (:foreground ,*string*))))
+   `(font-lock-type-face ((t (:foreground ,*type-face*))))
+   `(font-lock-variable-name-face ((t (:foreground ,*variable*))))
+   `(font-lock-warning-face ((t (:foreground ,*warning*))))
+
+   ;; GUI
+   `(fringe ((t (:background ,*background*))))
+   `(linum ((t (:background ,*line-number* :foreground ,*line-fg*))))
+   `(minibuffer-prompt ((t (:foreground ,*variable*))))
+   `(mode-line ((t (:background ,*mode-line-bg* :foreground ,*mode-line-fg*))))
+   `(mode-line-inactive ((t (:background ,*mode-inactive-bg* :foreground ,*mode-inactive-fg*))))
+   `(cursor ((t (:background ,*cursor-underscore*))))
+   `(text-cursor ((t (:background ,*cursor-underscore*))))
+   `(vertical-border ((t (:foreground ,*background*)))) ;; between splits
+
+   ;; show-paren
+   `(show-paren-mismatch ((t (:background ,*warning* :foreground ,*normal* :weight bold))))
+   `(show-paren-match ((t (:background ,*keywords* :foreground ,*normal* :weight bold))))
+
+   ;; search
+   `(isearch ((t (:background ,*regexp* :foreground ,*visual-selection*))))
+   `(isearch-fail ((t (:background ,*warning*))))
+   `(lazy-highlight ((t (:background ,*operators* :foreground ,*visual-selection*))))
+   ))
+
+(provide-theme 'witness)
+
+;;; witness-theme.el ends here
