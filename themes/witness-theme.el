b976af1b61e8d6f916f78645fc6f7d00770c47e9
Matt Lee <thatismatt@gmail.com>
2014-04-29 15:10:59 +0100

    Theme colour tweaks.

--- themes/witness-theme.el
+++ themes/witness-theme.el
@@ -22,16 +22,19 @@
        (*grey-2*             "#222")
        (*grey-3*             "#333")
        (*grey-4*             "#444")
+       (*grey-7*             "#777")
        (*grey-8*             "#888")
        (*grey-9*             "#999")
        (*grey-a*             "#AAA")
        (*red*                "#810")
-       (*red-1*              "#600")
+       (*red-1*              "#b10")
+       (*yellow*             "#f90")
        (*green*              "#080")
        (*pink*               "#c0a")
        (*purple*             "#93e")
        (*purple-1*           "#609")
        (*blue*               "#18e")
+       (*blue-1*             "#3ae")
 
        (*background*         "#011")
        (*normal*             *grey-a*)
@@ -42,17 +45,17 @@
        (*visual-selection-2* "#123")
        (*visual-selection-3* "#328")
 
-       (*comments*           "#555")
+       (*comments*           *grey-7*)
        (*shadow*             *purple-1*)
        (*keywords*           *purple*)
        (*constant*           *pink*)
        (*type-face*          *blue*)
        (*method-declaration* *blue*)
-       (*number*             "#00f")
+       (*number*             *blue-1*)
        (*builtins*           *pink*)
        (*warning*            *red-1*)
        (*regexp*             "#a36")
-       (*search-1*           "#f90")
+       (*search-1*           *yellow*)
        (*search-2*           "#a30")
        (*string*             *green*)
        (*variable*           "#0a8"))
@@ -129,9 +132,20 @@
    `(speedbar-selected-face ((t (:foreground ,*pink*))))
    `(speedbar-highlight-face ((t (:foreground ,*grey-a*))))
    `(speedbar-button-face ((t (:foreground ,*green*))))
+   `(speedbar-separator-face ((t (:foreground ,*grey-4*))))
 
    ;; org
-   `(org-code ((t (:foreground ,*purple*))))
+   `(org-level-1 ((t (:foreground ,*blue*))))
+   `(org-code ((t (:foreground ,*green*))))
+   `(org-table ((t (:foreground ,*blue-1*)))))
+   `(org-document-title ((t (:foreground ,*yellow*))))
+   `(org-document-info-keyword ((t (:foreground ,*purple*))))
+
+   ;; eshell
+   `(eshell-prompt ((t (:foreground ,*pink*))))
+
+   ;; shell
+   `(sh-heredoc ((t (:foreground ,*green*))))
    ))
 
 (provide-theme 'witness)
