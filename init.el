9b329ebce05c89cd76acc9821fb4bcd96b8f033e
Matt Lee <thatismatt@gmail.com>
2014-05-28 18:50:05 +0100

    Ibuffer tweaks.
    - Increase width of the name column.
    - Change default sort of buffers to alphabetically.

--- init.el
+++ init.el
@@ -58,6 +58,16 @@
 ;(put 'narrow-to-region 'disabled nil)
 ;(put 'narrow-to-page 'disabled nil)
 
+;; ibuffer
+(require 'ibuffer)
+(setq ibuffer-default-sorting-mode 'alphabetic)
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 25 25 :left :elide) " "
+              (size 9 -1 :right) " "
+              (mode 16 16 :left :elide) " " filename-and-process)
+        (mark " " (name 16 -1) " " filename)))
+
 ;; smartparens
 (require 'smartparens)
 (setq sp-highlight-pair-overlay nil)
