83f6dd3bc57b53d2142d8fc06187e3ee97e63b57
Matt Lee <thatismatt@gmail.com>
2014-07-11 14:46:09 +0100

    Theme tweaks and tidying.

--- themes/witness-theme.el
+++ themes/witness-theme.el
@@ -25,9 +25,8 @@
        (*grey-7*             "#777")
        (*grey-8*             "#888")
        (*grey-9*             "#999")
-       (*grey-a*             "#AAA")
+       (*grey-a*             "#aaa")
        (*red*                "#b10")
-       (*red-1*              "#810")
        (*yellow*             "#f90")
        (*green*              "#080")
        (*pink*               "#c0a")
@@ -50,13 +49,18 @@
        (*type-face*          *blue-1*)
        (*function*           *blue*)
        (*number*             *blue-1*)
+       (*string*             *green*)
        (*builtins*           *pink*)
        (*warning*            *red*)
        (*regexp*             "#a36")
        (*search-1*           *yellow*)
        (*search-2*           "#a30")
        (*variable*           "#0a8")
-       (*variable-2*         "#585"))
+       (*variable-2*         "#585")
+       (*highlight-1*        *blue*)
+       (*highlight-2*        *pink*)
+       (*highlight-3*        *yellow*)
+       )
 
   (custom-theme-set-faces
    'witness
@@ -66,12 +70,10 @@
    `(default ((t (:background ,*background* :foreground ,*normal*))))
    `(header-line ((t (:foreground ,*constant*))))
    `(highlight ((t (:background ,*background*))))
-   `(highlight-face ((t (:background ,*background*))))
    `(hl-line ((t (:weight bold))))
    `(info-xref ((t (:foreground ,*purple* :underline t))))
    `(region ((t (:background ,*visual-selection*))))
    `(secondary-selection ((t (:background ,*visual-selection-2*))))
-   `(shadow ((t (:foreground ,*purple-1*))))
    `(underline ((nil (:underline t))))
 
    ;; font-lock
@@ -89,7 +91,7 @@
    `(font-lock-reference-face ((t (:foreground ,*constant*))))
    `(font-lock-regexp-grouping-backslash ((t (:foreground ,*regexp*))))
    `(font-lock-regexp-grouping-construct ((t (:foreground ,*regexp*))))
-   `(font-lock-string-face ((t (:foreground ,*green*))))
+   `(font-lock-string-face ((t (:foreground ,*string*))))
    `(font-lock-type-face ((t (:foreground ,*type-face*))))
    `(font-lock-variable-name-face ((t (:foreground ,*variable*))))
    `(font-lock-warning-face ((t (:foreground ,*warning* :slant italic))))
@@ -110,7 +112,7 @@
    `(show-paren-mismatch ((t (:background ,*warning* :foreground ,*normal* :weight bold))))
    `(show-paren-match ((t (:background ,*visual-selection-3* :foreground ,*normal* :weight bold))))
 
-   ;; smart-parens
+   ;; smartparens
    `(sp-show-pair-match-face ((t (:background ,*visual-selection-3*))))
 
    ;; rainbow delimiters
@@ -129,7 +131,7 @@
    `(diff-header ((t (:background ,*visual-selection-1*))))
    `(diff-file-header ((t (:background ,*visual-selection-1*))))
    `(diff-added ((t (:foreground ,*green* :background nil))))
-   `(diff-removed ((t (:foreground ,*red-1* :background nil))))
+   `(diff-removed ((t (:foreground ,*red* :background nil))))
 
    ;; ediff
    `(ediff-even-diff-A ((t (:foreground ,*grey-8* :background ,*visual-selection-2*))))
@@ -138,14 +140,14 @@
    `(ediff-odd-diff-B ((t (:foreground ,*grey-a* :background ,*visual-selection-2*))))
 
    ;; magit
-   `(magit-branch ((t (:foreground ,*blue*))))
-   `(magit-log-sha1 ((t (:foreground ,*pink*))))
-   `(magit-tag ((t (:foreground ,*yellow* :background nil))))
+   `(magit-branch ((t (:foreground ,*highlight-1*))))
+   `(magit-log-sha1 ((t (:foreground ,*highlight-2*))))
+   `(magit-tag ((t (:foreground ,*highlight-3* :background nil))))
 
    ;; speedbar
    `(speedbar-file-face ((t (:foreground ,*purple*))))
    `(speedbar-directory-face ((t (:foreground ,*purple*))))
-   `(speedbar-selected-face ((t (:foreground ,*pink*))))
+   `(speedbar-selected-face ((t (:foreground ,*highlight-2*))))
    `(speedbar-highlight-face ((t (:foreground ,*grey-a*))))
    `(speedbar-button-face ((t (:foreground ,*green*))))
    `(speedbar-separator-face ((t (:foreground ,*grey-4*))))
@@ -158,15 +160,27 @@
    `(org-document-info-keyword ((t (:foreground ,*purple*))))
 
    ;; eshell
-   `(eshell-prompt ((t (:foreground ,*pink*))))
+   `(eshell-prompt ((t (:foreground ,*highlight-2*))))
 
    ;; shell
    `(sh-heredoc ((t (:foreground ,*green*))))
-   `(sh-quoted-exec ((t (:foreground ,*pink*))))
+   `(sh-quoted-exec ((t (:foreground ,*highlight-2*))))
 
    ;; scala
    `(scala-font-lock:var-face ((t (:foreground ,*variable-2* :inherit nil))))
+
+   ;; web
+   `(web-mode-html-tag-face ((t (:foreground ,*function*))))
+   `(web-mode-html-attr-name-face ((t (:foreground ,*variable*))))
+   `(web-mode-block-attr-name-face ((t (:foreground ,*variable*))))
    ))
+
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
 
 (provide-theme 'witness)
 
