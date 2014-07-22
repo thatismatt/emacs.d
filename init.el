69d635e34b8ec95bcc4adc099a74bef2df5914ea
Matt Lee <thatismatt@gmail.com>
2014-07-22 20:55:22 +0100

    Migrate away from Emacs Prelude.

--- init.el
+++ init.el
@@ -5,31 +5,92 @@
 
 ;;; Code:
 
-(prelude-require-packages
- '(
-   yasnippet
-   js2-mode
-   js2-refactor
-   sbt-mode
-   ))
+(require 'cl)
+(require 'package)
+(require 'dash)
 
-(disable-theme 'zenburn)
+(add-to-list 'load-path (expand-file-name "." user-emacs-directory))
 
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
+(defvar matt-packages
+  '(
+    ;;anzu
+    browse-kill-ring
+    company
+    diff-hl
+    elisp-slime-nav
+    gitconfig-mode
+    gitignore-mode
+    projectile
+    magit
+    move-text
+    rainbow-delimiters
+    rainbow-mode
+    smartparens
+    smartrep
+    smex
+    undo-tree
+    yasnippet
+    js2-mode
+    js2-refactor
+    sbt-mode
+    ido-ubiquitous
+    flx-ido
+    markdown-mode
+    ;;web-mode
+    ;;gitgutter?
+    ;;scala
+    )
+  "A list of packages to ensure are installed at launch.")
+
+(defun matt-packages-installed-p ()
+  "Check if all packages in `matt-packages' are installed."
+  (every 'package-installed-p matt-packages))
+
+(defun matt-require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (memq package matt-packages)
+    (add-to-list 'matt-packages package))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun matt-require-packages (packages)
+  "Ensure PACKAGES are installed. Missing packages are installed automatically."
+  (mapc 'matt-require-package packages))
+
+(defun matt-install-packages ()
+  "Install all packages listed in `matt-packages'."
+  (unless (matt-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Matt's Emacs is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (matt-require-packages matt-packages)))
+
+(matt-install-packages)
+
+;; hide tool bar & menu bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; themes
 (setq custom-theme-load-path
-      (cons "~/.emacs.d/personal/themes/" custom-theme-load-path))
+      (cons "~/.emacs.d/themes/" custom-theme-load-path))
 (load-theme 'witness t)
 
-(setq prelude-guru nil)
-(setq prelude-whitespace nil)
-(setq prelude-flyspell nil)
-
 ;; font
-(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil :height 130)
 ;; (set-face-attribute 'default nil :family "Nimbus Mono L")
-(set-face-attribute 'default nil :family "Liberation Mono")
+;; (set-face-attribute 'default nil :family "Liberation Mono")
 ;; (set-face-attribute 'default nil :family "DejaVu Sans Mono")
 ;; (set-face-attribute 'default nil :family "Droid Sans Mono")
-;; (set-face-attribute 'default nil :family "Inconsolata")
+(set-face-attribute 'default nil :family "Inconsolata")
 ;; (set-face-attribute 'default nil :family "Monofur")
 ;; (set-face-attribute 'default nil :family "Ubuntu Mono")
 ;; (set-face-attribute 'default nil :family "Andale Mono")
@@ -56,10 +117,54 @@
 ;; turn off newline on save
 (setq require-final-newline nil)
 
+;; spaces, not tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 8) ;; but maintain correct appearance
+
+;; auto revert
+(global-auto-revert-mode t)
+
+;; y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; delete the selection with a keypress
+(delete-selection-mode t)
+
+;; window splitting - always horizontal
+(setq split-height-threshold 0)
+(setq split-width-threshold nil)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 ;; enable stuff
 ;(put 'upcase-region 'disabled nil)
 ;(put 'narrow-to-region 'disabled nil)
 ;(put 'narrow-to-page 'disabled nil)
+
+(require 'smex)
+;; (setq smex-save-file (expand-file-name ".smex-items" prelude-savefile-dir))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+(require 'ido)
+(require 'flx-ido)
+(setq ido-enable-prefix nil)
+(setq ido-enable-flex-matching t)
+(setq ido-create-new-buffer 'always)
+(setq ido-use-filename-at-point 'guess)
+(setq ido-max-prospects 10)
+(setq ido-save-directory-list-file (expand-file-name "ido.hist" user-emacs-directory))
+(setq ido-default-file-method 'selected-window)
+(setq ido-auto-merge-work-directories-length -1)
+(setq ido-use-faces nil)
+(ido-mode +1)
+(ido-ubiquitous-mode +1)
+(flx-ido-mode +1)
 
 (require 'ibuffer)
 (setq ibuffer-default-sorting-mode 'alphabetic)
@@ -69,25 +174,79 @@
               (size 9 -1 :right) " "
               (mode 16 16 :left :elide) " " filename-and-process)
         (mark " " (name 16 -1) " " filename)))
+(defun ibuffer-ido-find-file (file &optional wildcards)
+  "Like `ido-find-file', but default to the directory of the buffer at point."
+  (interactive
+   (let ((default-directory
+           (let ((buf (ibuffer-current-buffer)))
+             (if (buffer-live-p buf)
+                 (with-current-buffer buf
+                   default-directory)
+               default-directory))))
+     (list (ido-read-file-name "Find file: " default-directory) t)))
+  (find-file file wildcards))
+(define-key ibuffer-mode-map "\C-x\C-f" 'ibuffer-ido-find-file)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-save-file (expand-file-name "recentf" user-emacs-directory)
+      recentf-max-saved-items 500
+      recentf-max-menu-items 15
+      recentf-auto-cleanup 'never) ;; disable - can cause problems with remote files
+(defun matt-recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: "
+                                   (-map 'abbreviate-file-name recentf-list)
+                                   nil t)))
+    (when file
+      (find-file file))))
+(global-set-key (kbd "C-x f") 'matt-recentf-ido-find-file)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
+(require 'undo-tree)
+(global-undo-tree-mode)
 
 (require 'smartparens)
+(require 'smartparens-config)
 (setq sp-highlight-pair-overlay nil)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+(show-smartparens-global-mode +1)
+(add-hook 'prog-mode-hook 'smartparens-mode)
+
+(require 'company)
+(setq company-idle-delay 0.5)
+(setq company-tooltip-limit 10)
+(setq company-minimum-prefix-length 2)
+(setq company-tooltip-flip-when-above t)
+(global-company-mode 1)
 
 (require 'linum)
 (setq linum-format "%4i ")
 (add-hook 'prog-mode-hook 'linum-mode)
 (add-hook 'css-mode-hook 'linum-mode)
+(add-hook 'html-mode-hook 'linum-mode)
 
 (require 'yasnippet)
-(add-hook 'prog-mode-hook
-          '(lambda () (yas-minor-mode)))
-;;(setq yas-snippet-dirs '("~/.emacs.d/personal/snippets" "~/.emacs.d/elpa/yasnippet-20140106.1009/snippets"))
-;;(setq yas-snippet-dirs '("~/.emacs.d/personal/snippets"))
-(setq yas-snippet-dirs (cons "~/.emacs.d/personal/snippets" (cdr yas-snippet-dirs)))
+(yas-global-mode)
+;;(setq yas-snippet-dirs '("~/.emacs.d/snippets" "~/.emacs.d/elpa/yasnippet-20140106.1009/snippets"))
+;;(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+(setq yas-snippet-dirs (cons "~/.emacs.d/snippets" (cdr yas-snippet-dirs)))
 ;; HACK - relies on the second element of yas-snippets-dir being the snippets from melpa
 ;; NOTE - yas fails to load if a dir that doesn't exist is in yas-snippets-dir
 ;;        ~/.emacs.d/snippets is in there by default, and doesn't exist
 (yas-reload-all)
+
+(require 'projectile)
+(projectile-global-mode)
 
 (require 'speedbar)
 (setq speedbar-show-unknown-files t)
@@ -99,8 +258,18 @@
       (kill-buffer " SPEEDBAR")))
 
 (require 'magit)
+;;(setq magit-diff-options '("--ignore-space-change"))
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 ;;(setq magit-server-window-for-commit nil)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(require 'diff-hl)
+(global-diff-hl-mode +1)
+(add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+(global-set-key (kbd "C-c g") diff-hl-command-map)
+
+(require 'ediff)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain) ;; don't start another frame
 
 (require 'org)
 (setq org-todo-keywords '((type "TODO" "DOING" "DONE" "|" "POSTPONED")))
@@ -122,36 +291,43 @@
 (define-key org-mode-map (kbd "C-<left>") nil)
 (define-key org-mode-map (kbd "C-<right>") nil)
 
-;(set-face-background 'hl-line "#323")
-;(set-face-attribute 'hl-line nil :underline nil)
+(defun matt-font-lock-comment-annotations ()
+  "Highlight well known comment annotations."
+  (font-lock-add-keywords
+   nil '((" \\<\\(\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\)\\)[: ]"
+          1 font-lock-warning-face t))))
+(add-hook 'prog-mode-hook 'matt-font-lock-comment-annotations)
 
-(setq whitespace-style '(face tabs empty trailing))
+(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-mode)
 
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(setq js2-global-externs '("setTimeout" "setInterval" "require" "exports" "$" "angular" "_" "Highcharts" "google" "d3" "chai" "it" "describe" "suite" "test"))
+(setq js2-global-externs
+      '("setTimeout" "setInterval" "require" "module" "exports" "$" "angular" "_"
+        "Highcharts" "google" "d3" "chai" "it" "describe" "suite" "test"))
 (require 'js2-refactor)
 (js2r-add-keybindings-with-prefix "C-c C-m")
-
-(require 'web-mode)
-(setq web-mode-indent-style 2)
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
+(define-key js2-mode-map (kbd "<C-S-down>") nil)
+(define-key js2-mode-map (kbd "<C-S-up>") nil)
 
 (require 'css-mode)
 (setq css-indent-offset 4)
+(add-hook 'css-mode-hook 'rainbow-mode)
 
-(setq sbt:program-name "~/opt/sbt/sbt")
-(add-hook 'sbt-mode-hook '(lambda ()
-  (setq compilation-skip-threshold 1)
-  (local-set-key (kbd "C-a") 'comint-bol)))
-(add-hook 'scala-mode-hook '(lambda ()
-   (local-set-key (kbd "M-.") 'sbt-find-definitions)
-   (local-set-key (kbd "C-x '") 'sbt-run-previous-command)
-   (local-set-key (kbd "C-M-x") 'sbt-send-region)))
+;;(require 'scala)
+;;(setq sbt:program-name "~/opt/sbt/sbt")
+;;(add-hook 'sbt-mode-hook '(lambda ()
+;;  (setq compilation-skip-threshold 1)
+;;  (local-set-key (kbd "C-a") 'comint-bol)))
+;;(add-hook 'scala-mode-hook '(lambda ()
+;;   (local-set-key (kbd "M-.") 'sbt-find-definitions)
+;;   (local-set-key (kbd "C-x '") 'sbt-run-previous-command)
+;;   (local-set-key (kbd "C-M-x") 'sbt-send-region)))
 
 ;; auto mode
-(add-to-list 'auto-mode-alist '("\\.\\(jspf?\\|tag\\)$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(jspf?\\|tag\\)$" . html-mode))
 
 ;; window & buffer switching
 (defun matt-normal-buffer-switch (f)
@@ -166,10 +342,6 @@
 (define-key global-map (kbd "<menu>")
   (lambda () (interactive) (matt-normal-buffer-switch 'next-buffer)))
 (define-key global-map (kbd "C-<menu>") 'other-window)
-
-;; window splitting - always horizontal
-(setq split-height-threshold 0)
-(setq split-width-threshold nil)
 
 (defun matt-toggle-window-dedicated ()
   "Toggle whether the current active window is dedicated or not"
@@ -214,12 +386,12 @@
 
 (defun matt-open-init ()
   (interactive)
-  (find-file "~/.emacs.d/personal/init.el"))
+  (find-file "~/.emacs.d/init.el"))
 
 (defun matt-open-theme ()
   (interactive)
   (let ((theme (car custom-enabled-themes)))
-    (find-file (format "~/.emacs.d/personal/themes/%s-theme.el" theme))))
+    (find-file (format "~/.emacs.d/themes/%s-theme.el" theme))))
 
 (defun matt-open-todo ()
   (interactive)
@@ -263,5 +435,8 @@
 (global-set-key (kbd "S-<right>") 'next-buffer)
 (global-set-key (kbd "S-<left>") 'previous-buffer)
 
-;; key chords
-(key-chord-define-global "xx" nil)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-o") 'other-window)
+
+(global-set-key (kbd "C-S-<up>") 'move-text-up)
+(global-set-key (kbd "C-S-<down>") 'move-text-down)
