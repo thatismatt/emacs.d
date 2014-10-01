693c63cf4e99436f08a5c6e040b508afa233109f
Matt Lee <thatismatt@gmail.com>
2014-10-01 15:13:01 +0100

    Turn on eldoc.

--- init.el
+++ init.el
@@ -299,6 +299,7 @@
           1 font-lock-warning-face t))))
 (add-hook 'prog-mode-hook 'matt-font-lock-comment-annotations)
 
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-mode)
