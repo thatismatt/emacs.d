8e62cb8d532051c2d05e9308fe1be4e9d1a85436
Matt Lee <thatismatt@gmail.com>
2015-05-12 14:17:54 +0100

    Open HTML files in web mode.

--- init.el
+++ init.el
@@ -394,6 +394,7 @@
 (setq web-mode-code-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(jspf?\\|tag\\)$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[sx]?html?\\(\\.[a-zA-Z_]+\\)?\\'" . web-mode))
 
 (require 'less-css-mode)
 
