52527a0e36acc7bb5c52fb08465e76b23f3211bb
Matt Lee <thatismatt@gmail.com>
2015-07-24 16:44:51 +0100

    Open file as root if not owned by current user.

--- init.el
+++ init.el
@@ -216,6 +216,27 @@
 (define-key ibuffer-name-map [(mouse-1)] 'ibuffer-mouse-visit-buffer)
 (define-key ibuffer-name-map [(mouse-2)] 'ibuffer-mouse-toggle-mark)
 
+;; open as root - from emacs prelude
+(defun matt-file-owner-uid (filename)
+  "Return the UID of the FILENAME as an integer. See `file-attributes' for more info."
+  (nth 2 (file-attributes filename 'integer)))
+(defun matt-file-owned-by-user-p (filename)
+  "Return t if file FILENAME is owned by the currently logged in user."
+  (equal (matt-file-owner-uid filename)
+         (user-uid)))
+(defun matt-find-alternate-file-as-root (filename)
+  "Wraps `find-alternate-file' with opening a file as root."
+  (find-alternate-file (concat "/sudo:root@localhost:" filename)))
+(defun matt-file-reopen-as-root ()
+  "Find file as root if necessary."
+  (unless (or (tramp-tramp-file-p buffer-file-name)
+              (equal major-mode 'dired-mode)
+              (not (file-exists-p (file-name-directory buffer-file-name)))
+              (file-writable-p buffer-file-name)
+              (matt-file-owned-by-user-p buffer-file-name))
+    (matt-find-alternate-file-as-root buffer-file-name)))
+(add-hook 'find-file-hook 'matt-file-reopen-as-root)
+
 (require 'recentf)
 (setq recentf-save-file (expand-file-name "recentf" user-emacs-directory))
 (setq recentf-max-saved-items 500)
