af21de6345a34296650dd166ca1ef474be6bf328
Matt Lee <thatismatt@gmail.com>
2015-12-02 14:06:00 +0000

    Turn off web-mode comment highlighting.

--- init.el
+++ init.el
@@ -402,6 +402,7 @@
 (require 'web-mode)
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
+(setq web-mode-enable-comment-interpolation nil)
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(jspf?\\|tag\\)$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[sx]?html?\\(\\.[a-zA-Z_]+\\)?\\'" . web-mode))
