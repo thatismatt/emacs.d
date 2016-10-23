e5f8309323318344c80946b0a70e55b917fe5aab
Matt Lee <thatismatt@gmail.com>
2016-10-23 10:26:47 +0100

    Rebind C-d to backward-delete-char.

--- init.el
+++ init.el
@@ -224,6 +224,7 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (delete-selection-mode t) ;; delete the selection with a keypress
 (savehist-mode)
+(global-set-key (kbd "C-d") 'backward-delete-char)
 
 ;; window splitting - always horizontal
 (setq split-height-threshold 0)
