7b848e68a801ad403b6858d3ebed6e99084debc8
Matt Lee <thatismatt@gmail.com>
2017-08-11 17:34:51 +0100

    Save cider repl history to file between sessions.

--- init.el
+++ init.el
@@ -491,6 +491,8 @@
 (add-hook 'cider-repl-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'smartparens-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+(setq cider-repl-history-size 1000)
+(setq cider-repl-history-file ".cider-repl-history") ;; local file, to get per project history
 ;; hook figwheel in to cider
 (setq cider-cljs-lein-repl
       "(do (require 'figwheel-sidecar.repl-api)
