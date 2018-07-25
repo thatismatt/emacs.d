640ef82e8bcde1c986624d80fe4aebf4116b205f
Matt Lee <thatismatt@gmail.com>
2018-07-25 17:18:52 +0100

    Add another time format to `matt-insert-time`.

--- init.el
+++ init.el
@@ -674,7 +674,7 @@
 ;; " Wednesday February 20 02/07/18 %E 2018-02-07 2018 15 03 %J %K %L 28 312138752 %O pm %Q 15:28 35 15:28:35 05 06 06 15:28:35 2018 GMT Wed Feb Wed 07 Feb 2018 15:28:35 GMT 07  7 %f 18 Feb %i 038 15  3 02
 ;;  %o pm 1  3:28:35 pm GMT 1518017315 	 3 %v 3 07/02/18 18 +0000"
 (defun matt-insert-date (arg)
-  "Insert the current date. e.g 2017-11-02"
+  "Insert the current date. e.g 2018-02-08, 2018 08-Feb-2018 or Thu, 08 February."
   (interactive "p")
   (insert (format-time-string
            (cond
@@ -685,10 +685,13 @@
 (matt-define-key "i d" 'matt-insert-date)
 
 (defun matt-insert-time (arg)
-  "Insert the current time. e.g 16:34:42 or 2018-02-08T16:34:42+0000"
-  (interactive "P")
+  "Insert the current time. e.g 16:34:42, 2018-02-08T16:34:42+0000 or 2018-02-08_16-34-42."
+  (interactive "p")
   (insert (format-time-string
-           (if arg "%FT%T%z" "%T")
+           (cond
+            ((eq arg 16) "%Y-%m-%d_%H-%M-%S") ;; 2018-02-08_16-34-42
+            ((eq arg 4)  "%FT%T%z")           ;; 2018-02-08T16:34:42+0000
+            (t           "%T"))               ;; 16:34:42
            (current-time))))
 (matt-define-key "i t" 'matt-insert-time)
 
