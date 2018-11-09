cce44133c1a57a4f929084a87421171c07e1112b
Matt Lee <thatismatt@gmail.com>
2018-11-09 15:38:55 +0000

    Add cider keybinding for cider-pprint-eval-last-sexp-to-comment.

--- init.el
+++ init.el
@@ -512,6 +512,7 @@
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 (setq cider-repl-history-size 1000)
 (setq cider-repl-history-file (expand-file-name ".cider-repl-history" user-emacs-directory))
+(define-key cider-mode-map (kbd "C-c ;") 'cider-pprint-eval-last-sexp-to-comment)
 ;; hook figwheel in to cider
 (setq cider-cljs-lein-repl
       "(do (require 'figwheel-sidecar.repl-api)
