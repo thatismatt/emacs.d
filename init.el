4cf2461f83dfd63863759321ec7a49375c1ef0ca
Matt Lee <thatismatt@gmail.com>
2019-08-08 17:40:04 +0100

    Remove uniquify, as now using git-buffer-naming.

--- init.el
+++ init.el
@@ -358,12 +358,6 @@
   (find-file file))
 (global-set-key (kbd "C-x f") 'matt-recentf-ido-find-file)
 
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-separator "/")
-(setq uniquify-after-kill-buffer-p t) ;; rename after killing uniquified
-(setq uniquify-ignore-buffers-re "^\\*") ;; don't muck with special buffers
 (use-package bookmark
   :bind (:map matt-keymap
               ("b m" . bookmark-set)
