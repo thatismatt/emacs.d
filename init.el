2a23917c54465df2a0a29ddaadb749e5e8f3123c
Matt Lee <thatismatt@gmail.com>
2020-05-06 09:59:28 +0100

    Move tramp to use-package.

--- init.el
+++ init.el
@@ -347,27 +347,31 @@
               ("o e" . eshell)
               ("o d" . matt-eshell-dwim-directory)))
 
-(require 'tramp)
-;; open as root - from emacs prelude
-(defun matt-file-owner-uid (filename)
-  "Return the UID of the FILENAME as an integer. See `file-attributes' for more info."
-  (nth 2 (file-attributes filename 'integer)))
-(defun matt-file-owned-by-user-p (filename)
-  "Return t if file FILENAME is owned by the currently logged in user."
-  (equal (matt-file-owner-uid filename)
-         (user-uid)))
-(defun matt-find-alternate-file-as-root (filename)
-  "Wraps `find-alternate-file' with opening a file as root."
-  (find-alternate-file (concat "/sudo:root@localhost:" filename)))
-(defun matt-file-reopen-as-root ()
-  "Find file as root if necessary."
-  (interactive)
-  (unless (or (tramp-tramp-file-p buffer-file-name)
-              (equal major-mode 'dired-mode)
-              (not (file-exists-p (file-name-directory buffer-file-name)))
-              (file-writable-p buffer-file-name)
-              (matt-file-owned-by-user-p buffer-file-name))
-    (matt-find-alternate-file-as-root buffer-file-name)))
+(use-package tramp
+  :defer t
+  :init
+  (autoload 'matt-file-reopen-as-root "tramp" nil t)
+  :config
+  ;; open as root - from emacs prelude
+  (defun matt-file-owner-uid (filename)
+    "Return the UID of the FILENAME as an integer. See `file-attributes' for more info."
+    (nth 2 (file-attributes filename 'integer)))
+  (defun matt-file-owned-by-user-p (filename)
+    "Return t if file FILENAME is owned by the currently logged in user."
+    (equal (matt-file-owner-uid filename)
+           (user-uid)))
+  (defun matt-find-alternate-file-as-root (filename)
+    "Wraps `find-alternate-file' with opening a file as root."
+    (find-alternate-file (concat "/sudo:root@localhost:" filename)))
+  (defun matt-file-reopen-as-root ()
+    "Find file as root if necessary."
+    (interactive)
+    (unless (or (tramp-tramp-file-p buffer-file-name)
+                (equal major-mode 'dired-mode)
+                (not (file-exists-p (file-name-directory buffer-file-name)))
+                (file-writable-p buffer-file-name)
+                (matt-file-owned-by-user-p buffer-file-name))
+      (matt-find-alternate-file-as-root buffer-file-name))))
 
 (require 'recentf)
 (setq recentf-save-file (expand-file-name "recentf" user-emacs-directory))
