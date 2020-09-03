edfecf311d47e635a0da17f65b7183f0d4f09afe
Matt Lee <thatismatt@gmail.com>
2020-09-03 16:40:19 +0100

    Font lock "DEBUG" comments.

--- init.el
+++ init.el
@@ -730,7 +730,7 @@
 (defun matt-font-lock-comment-annotations ()
   "Highlight well known comment annotations."
   (font-lock-add-keywords
-   nil '((" \\<\\(\\(FIX\\|TODO\\|NOTE\\|HACK\\|XXX\\|\\?\\?\\?\\)\\)[: ]"
+   nil '((" \\<\\(\\(FIX\\|TODO\\|NOTE\\|HACK\\|DEBUG\\|XXX\\|\\?\\?\\?\\)\\)[: ]"
           1 font-lock-warning-face t))))
 (add-hook 'prog-mode-hook 'matt-font-lock-comment-annotations)
 (add-hook 'web-mode-hook 'matt-font-lock-comment-annotations)
