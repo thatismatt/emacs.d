6957a14592d80a1c185ccdde31d0a669247b7ba3
Matt Lee <thatismatt@gmail.com>
2022-12-01 13:46:30 +0000

    Add matt-execute-buffer.

--- init.el
+++ init.el
@@ -1390,6 +1390,14 @@ e.g. 2020012016131337, 2020-01-20_16-13-13, 1621854380123 or 1621854380."
     (chmod filename
            (file-modes-symbolic-to-number "+x" (file-modes filename)))))
 
+(defun matt-execute-buffer ()
+  "Run this buffer as an async shell command."
+  (interactive)
+  (let ((filename (expand-file-name (buffer-file-name))))
+    (if (file-executable-p filename)
+        (async-shell-command filename (concat "*Shell Buffer* " (abbreviate-file-name filename)))
+      (message "This buffer isn't associated with an executable file"))))
+
 (defun matt-clear-text-properties (begin end)
   "Remove text properties between BEGIN and END, defaults to the region."
   (interactive "r")
