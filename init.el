bb01b0422cafb4ffac23828d14bff752b871ff26
Matt Lee <thatismatt@gmail.com>
2026-05-09 13:34:10 +0100

    Set group of todo state faces.

--- init.el
+++ init.el
@@ -916,9 +916,9 @@ With prefix ARG also kill all unmodified file buffers."
 (use-package org
   :defer t
   :init
-  (defface org-doing-face '((t (:inherit org-todo))) "org mode face for DOING items")
-  (defface org-postponed-face '((t (:inherit org-done))) "org mode face for POSTPONED items")
-  (defface org-query-face '((t (:inherit org-todo))) "org mode face for ??? (query) items")
+  (defface org-doing-face '((t (:inherit org-todo))) "org mode face for DOING items" :group 'org)
+  (defface org-postponed-face '((t (:inherit org-done))) "org mode face for POSTPONED items" :group 'org)
+  (defface org-query-face '((t (:inherit org-todo))) "org mode face for ??? (query) items" :group 'org)
   (setq org-fontify-done-headline nil)
   (setq org-todo-keywords '((sequence "TODO" "DOING" "DONE")
                             (sequence "BLOCKED")
